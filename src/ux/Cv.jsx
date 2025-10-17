f5d046dee7e997573829015acfa61df9d8a26c7e
Nov-Phannak <novphannak140@gmail.com>
2025-10-17 23:34:59 +0700

    update code

--- src/ux/Cv.jsx
+++ src/ux/Cv.jsx
@@ -2,10 +2,10 @@ import Profile from "../assets/photos/Nov Phannak-cv-profile.jpg";
 
 function Cv() {
   return (
-    <div className="w-full  h-auto flex justify-center items-center  py-20">
-      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
+    <div className="w-full  h-auto flex justify-center items-center  py-10">
+      <div className="grid grid-cols-1  md:grid-cols-1 lg:grid-cols-1">
         <div className="col-span-1">
-          <div className="lg:w-130 md:w-200 w-90">
+          <div className="lg:w-130 md:w-170 w-90">
             <img src={Profile} alt="" />
           </div>
         </div>
